25c2bdcf98115d27d89c8580295a173afa877f7f
Yantriksh Vasoya <164139770+Yantriksh-Vasoya@users.noreply.github.com>
2026-04-06 11:57:17 +0530

    Update prediction.py

--- src/prediction.py
+++ src/prediction.py
@@ -1,1 +1,21 @@
+import pickle
+import numpy as np
 
+
+def load_model(model_path):
+    model = pickle.load(open(model_path, "rb"))
+    return model
+
+
+def predict(model, input_data):
+
+    input_data = np.array(input_data).reshape(1, -1)
+
+    prediction = model.predict(input_data)
+
+    try:
+        probability = model.predict_proba(input_data)[0][1]
+    except:
+        probability = None
+
+    return prediction[0], probability
